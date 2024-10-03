1251b0615ce5cc76f77d7cc1136a8d8910415767
Xedric magana <Xmagana38@gmail.com>
2024-10-03 10:44:54 +0800

    Update Maganap02.BasicOutput.cpp

--- Maganap02.BasicOutput.cpp
+++ Maganap02.BasicOutput.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main ()
 {
-	string age;
-	cout << "Enter your age\t:\t";
-	cin >> age;
-	cout << "Thank you!!";
+	cout << "Our lady of lourdes college foundation" << endl;
+	cout << "Collge of computer studies" << endl;
+	cout << "Bachelor of science in information technology 1-B" << endl;
+	cout << "Jan Xedric Magana" << endl;
+	cout << "Purok -6 Tulay na lupa labo, Camarines Norte" << endl;
 	return 0;
 }
