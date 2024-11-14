1bf621594d2622a5b554df0b95bb1d6f979fb993
Xedric magana <Xmagana38@gmail.com>
2024-11-14 10:46:07 +0800

    Update MaganaActivity01.cpp

--- MaganaActivity01.cpp
+++ MaganaActivity01.cpp
@@ -1,19 +1,1 @@
-#include <iostream>
-using namespace std;
 
-int main() {
-    double temperature;
-    
-    cout << "Enter the temperature in degrees: ";
-    cin >> temperature;
-    
-    if (temperature < 32) {
-        cout << "It's cold! Bring a heavy jacket." << endl;
-    } else if (temperature >= 32 && temperature <= 50) {
-        cout << "It's a bit chilly! Bring a light jacket." << endl;
-    } else {
-        cout << "It's warm! No jacket needed." << endl;
-    }
-
-    return 0;
-}
